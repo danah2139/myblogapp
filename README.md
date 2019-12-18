3da5cc3e86c4182af6bb7ec961b3d12b0ff2ac65
danah2139 <danah2139@gmail.com>
2019-12-18 20:11:31 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# myflaskapp
+# myblogapp
+
+Credit to Jose Portilla - Python and Flask: Create Websites using Flask [https://www.udemy.com/course/python-and-flask-bootcamp-create-websites-using-flask/learn/lecture/11096660?start=345#overview]
---
+++ myblog/models.py
@@ -0,0 +1,54 @@
+from myblog import db , login_manager
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from datetime import datetime
+
+# The user_loader decorator allows flask-login to load the current user
+# and grab their id.
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
+class User(db.Model , UserMixin):
+     # Create a table in the db
+    __tablename__ = 'users'
+    
+    id = db.Column(db.Integer,primary_key = True)
+    profile_image = db.Column(db.String(64),nullable = False , default = 'default_profile1.png') 
+    email = db.Column(db.String(64),unique = True,index = True)
+    username = db.Column(db.String(64),unique= True,index = True)
+    password_hash = db.Column(db.String(128))
+    # This connects BlogPosts to a User Author
+    # lazy set to true emits a SELECT statement when loading
+    posts = db.relationship('BlogPost',backref = 'author',lazy = True)
+
+    def __init__(self,email,username,password):
+        self.email = email
+        self.username = username
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self,password):
+        return check_password_hash(self.password_hash,password)
+
+    def __repr__(self): #for debuging
+        return f"Username {self.username}"
+
+class BlogPost(db.Model, UserMixin):    
+    users = db.relationship(User)
+
+    id = db.Column(db.Integer,primary_key=True)
+    # Notice how we connect the BlogPost to a particular author
+    user_id = db.Column(db.Integer,db.ForeignKey('users.id'),nullable = False)
+    date = db.Column(db.DateTime , nullable = False,default = datetime.utcnow)
+    title = db.Column(db.String(140),nullable=False)
+    text = db.Column(db.Text,nullable=False)
+
+
+    def __init__(self,title,text,user_id):
+        self.user_id = user_id
+        self.title = title
+        self.text = text
+    
+    def __repr__(self): #for debuging
+        return f"Post ID:  {self.id} --- Date: {self.date} --- {self.title}"
